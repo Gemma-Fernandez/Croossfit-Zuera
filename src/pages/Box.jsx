2d3f03f38f992a868c8efefc6c6fde0a03bc9cc8
Gemma Fernandez <gemmazuera@gmail.com>
2024-11-07 12:07:23 +0100

    box images

--- src/pages/Box.jsx
+++ src/pages/Box.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import baño1 from "../images/baños1.jpeg";
+import baño2 from "../images/baños2.jpeg";
+import baño3 from "../images/baños3.jpeg";
+import baño4 from "../images/baños4.jpeg";
+import baño5 from "../images/baños5.jpeg";
+import Carousel from "react-bootstrap/Carousel";
+import box1 from "../images/box1.jpeg";
+import box2 from "../images/box2.jpeg";
 
 function Box() {
   return (
-    <div>Box</div>
-  )
+    <div>
+      <div>
+        <Carousel fade>
+          <Carousel.Item>
+            <img src={baño1} alt="imagen baño" style={{ width: "500px" }} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={baño2} alt="imagen baño" style={{ width: "500px" }} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={baño3} alt="imagen baño" style={{ width: "500px" }} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={baño4} alt="imagen baño" style={{ width: "500px" }} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={baño5} alt="imagen baño" style={{ width: "500px" }} />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <br />
+      <div>
+        <Carousel fade>
+          <Carousel.Item>
+            <img src={box1} alt="imagen box" style={{ width: "500px" }} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={box2} alt="imagen box" style={{ width: "500px" }} />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  );
 }
 
-export default Box
+export default Box;
--- src/pages/Clases.jsx
+++ src/pages/Clases.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Clases() {
   return (
-    <div>Clases</div>
-  )
+    <div>
+      <h2>Nuestras clases</h2>
+      <h4>Endurance</h4>
+      <p>Clase para mejorar nuestra resistencia con el objetivo de lograr reducir los tiempos de recuperación, tener más potencia y minimizar el riesgo de lesiones.</p>
+      <h4>Pure Strength</h4>
+      <p></p>
+      <h4>Team Wod</h4>
+      <p></p>
+
+    </div>
+  );
 }
 
-export default Clases
+export default Clases;
